7b5095a3c774c01c7ed2456ee655edda79170c52
lehieulol <42810931+lehieulol@users.noreply.github.com>
2019-12-23 13:12:02 +0700

    Update getHS.js

--- getHS.js
+++ getHS.js
@@ -2,7 +2,7 @@ function getHS(){
 	let re = new XMLHttpRequest();
 	re.onreadystatechange = function(){
 		if(this.readyState = 4 && this.status = 200){
-			return this.responseXML;
+			return this.responseText;
 		}
 	}
 	re.open('GET','HS.xml',true);
